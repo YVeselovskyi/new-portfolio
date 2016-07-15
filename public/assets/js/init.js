3e1cfc1f8b7d467fd4b15cad840f008c870d593c
Yaroslav Veselovskyi <19dynamokyiv94@gmail.com>
2016-07-15 16:24:57 +0300

    CSS Animations

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -7,18 +7,3 @@
     }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-function displaySocial(){
-
-  var menu = document.getElementsByClassName('flaticon-menu')[0];
-
-  menu.addEventListener('click' , function(){
-    var socialMenu = document.getElementsByClassName('icons');
-    for(var i=0; i<socialMenu.length; i++){
-        socialMenu[i].classList.remove('hidden');
-    }
-    console.log('Hi')
-  })
-}
-
-
-displaySocial();
