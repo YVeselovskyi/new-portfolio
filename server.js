965f18db9e583f1309b846dd5227927221a29e29
Yaroslav Veselovskyi <19dynamokyiv94@gmail.com>
2016-07-15 14:44:00 +0300

    Bug fixed

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
+'use strict';
 const express = require('express');
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 1927;
+const ejs = require('ejs');
+const ejsMate = require('ejs-mate');
 
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/index.html'));
+
+
+app.engine('ejs' , ejsMate);
+app.set('view engine', 'ejs');
+
+app.get('/' , (req, res) => {
+  res.render('main/home');
 });
 
-app.get('/works', function (req, res) {
-  res.send('Test');
+app.get('/about' , (req, res) => {
+  res.render('main/about', {title: 'About'});
+});
+
+app.get('/works' , (req, res) => {
+  res.render('main/works', {title: 'Works'});
+});
+
+app.get('/contacts' , (req, res) => {
+  res.render('main/contacts', {title: 'Contacts'});
 });
 
 app.use('/', express.static(__dirname + '/public/assets'));
